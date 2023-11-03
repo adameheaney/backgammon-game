a1bc8bb110513706a07d8ff6eb069936dff08d71
AdamHeaney <94878093+Machooey@users.noreply.github.com>
2023-11-03 15:27:53 -0400

    compiled and still editing

--- src/Backgammon.java
+++ src/Backgammon.java
@@ -19,11 +19,16 @@ public class Backgammon {
     }
 
     public void startGame() {
-        System.out.println("Choose teams. When ready type anything and press enter");
-        System.out.println(b.boardString());
+        System.out.println("Welcome to the game of Backgammon! I will not be restating the rules as I assume you know them if you're playing this. The code for this game was created from scratch by Adam Heaney using only Java and dedication. \n\nHow to play: When it is your turn to move, input coordinates as follows: \"x y\". Here is what the board looks like:\n\n" + b.boardString() + "\nThe two teams are B and W (black and white). Black's home is the top row and White's is the bottom. E is how many of your pieces are eaten and I is how many of your pieces are home. When ready, pick teams, type anything, and press enter to begin the game!");
         console.nextLine();
         int[] rolls = d.rolls(2);
-        
+        while(rolls[0] == rolls[1])
+            rolls = d.rolls(2);
+        if(rolls[0] > rolls[1]) 
+            System.out.println(b.getTeams()[0].getTeamName() + " goes first!");
+        else 
+            System.out.println(b.getTeams()[1].getTeamName() + " goes first!");
+        System.out.println("Input the coordinates of the piece you'd like to move!");
         
     }
 
--- src/BackgammonRunner.java
+++ src/BackgammonRunner.java
@@ -2,7 +2,8 @@ package src;
 
 public class BackgammonRunner {
     public static void main(String[] args) {
-       
+        Backgammon game = new Backgammon();
+        game.startGame();
     }
 
 }
