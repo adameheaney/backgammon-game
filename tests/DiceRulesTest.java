e04c70cff57c7300068e80f47ee73e272e13098d
AdamHeaney <94878093+Machooey@users.noreply.github.com>
2023-12-11 21:20:15 -0500

    cleanup aisle 5

--- tests/DiceRulesTest.java
+++ tests/DiceRulesTest.java
@@ -10,42 +10,49 @@ public class DiceRulesTest {
         eatenPiecesTest();
     }
     
+    @SuppressWarnings({"unused"})
     private static void sameRollTest() {
         b = new BackgammonBoard("3 0", "11 0 11 0");
         game = new Backgammon(b);
         game.playTest(3, 5, 1);
     }
 
+    @SuppressWarnings({"unused"})
     private static void twoSingleMoveRolls() {
         b = new BackgammonBoard("3 0 2 0", "5 0 5 0 7 0 7 0 8 0 8 0");
         game = new Backgammon(b);
         game.playTest(2, 3, 1);
     }
 
+    @SuppressWarnings({"unused"})
     private static void doublesSingleMoveTest() {
         b = new BackgammonBoard("3 0 3 0 3 0", "9 0 9 0");
         game = new Backgammon(b);
         game.playTest(5, 5, 1);
     }
 
+    @SuppressWarnings({"unused"})
     private static void basicTest() {
         b = new BackgammonBoard();
         game = new Backgammon(b);
         game.playTest(3, 5, 2);
     }
 
+    @SuppressWarnings({"unused"})
     private static void diceAfterOtherDiceTest() {
         b = new BackgammonBoard("2 0", "1 1 1 1 3 1 3 1 7 0 7 0");
         game = new Backgammon(b);
         game.playTest(2, 5, 1);
     }
 
+    @SuppressWarnings({"unused"})
     private static void forcedMoveTest() {
         b = new BackgammonBoard("2 0 8 1", "1 1 1 1 3 1 3 1 7 0 7 0");
         game = new Backgammon(b);
         game.playTest(2, 5, 1);
     }
-
+    
+    @SuppressWarnings({"unused"})
     private static void eatenPiecesTest() {
         b = new BackgammonBoard("E E 8 1", "10 1 10 1");
         game = new Backgammon(b);
